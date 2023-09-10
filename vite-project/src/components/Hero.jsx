02446b4c9e7bf230506f413a7c504a6384b031f2
umair <umairqamar500@gmail.com>
2023-09-10 06:20:22 +0500

    add: initial files

---
+++ vite-project/src/components/Hero.jsx
@@ -0,0 +1,38 @@
+const Hero = () =>{
+    return(
+        <main className="hero containerfoff">
+<div className="hero-content">
+
+    <h1>YOUR FEET DESERVE THE BEST</h1>
+    <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP 
+    YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+
+<div className="hero-btn">
+    <button>shop now </button>
+    <button className="second-btn">cateogry </button>
+</div>
+
+<div className="shopping"> <p>Also available on</p>
+ </div>
+
+<div className="brand-logo">
+    <img src="/images/flipkart.png" alt="flip"/>
+    <img src="/images/amazon.png" alt="amaz"/>
+
+</div>
+
+
+</div>
+
+<div className="hero-image">
+<img src="/images/shoe_image.png" alt="shoe"/>
+ </div>
+
+
+
+</main>
+
+    );
+};
+
+export default Hero;
---
+++ vite-project/src/components/Navigation.jsx
@@ -0,0 +1,22 @@
+const Navigation = () =>{
+return(
+<nav className="container">
+<div className="logo">
+  <img src= "/images/brand_logo.png" alt="nike" />
+</div>
+
+<ul>
+  <li href="#">Menu</li>
+  <li href="#">location</li>
+  <li href="#">about</li>
+  <li href="#">contact</li>
+</ul>
+
+<button>login</button>
+
+  </nav>
+
+);
+};
+
+export default Navigation;
